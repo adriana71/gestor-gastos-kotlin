12800646c83debaac284aa043fe6936d1581f715
Adriana Rojas <adriana.rojas@uaq.edu.mx>
2026-08-12 07:45:42 -0600

    Inicializa proyecto Kotlin del gestor de gastos

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+package src
+
+fun main() {
+    println("Gestor semanal de gastos")
+}
+
+fun registrarGasto(
+    conceptos: MutableList<String>,
+    categorias: MutableList<String>,
+    montos: MutableList<Double>
+)
+{
+     //Declarar un arreglo con categorías disponibles
+     val categoriaDisponibles = arrayOf("Alimentos", "Transporte", "Materiales Escolares", "Entretenimiento", "Otros");
+     //Declarar variable concepto
+     //Solicitar el concepto con letrero de "Ingresa tu gasto"
+     //Mostrar categorías disponibles (arreglo con la categorías)
+    // Seguir haciendo pseudocodigo
+
+}
